8a7f018aae5521f3d3f8f70acc8c12fc5123d354
Anderson C Silva <andersoncsilvadev@gmail.com>
2025-03-24 07:53:03 -0300

    criaçao das classes das tabelas

---
+++ Comandas.Api/Comandas.Api/Models/Comanda.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Comandas.Api.Models
+{
+    public class Comanda
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int NumeroMesa { get; set; }
+
+        public string NomeCliente { get; set; }
+
+        public int SituacaoComanda { get; set; }
+        public virtual ICollection<ComandaItem> ComandaItems { get; set; }
+    }
+}
---
+++ Comandas.Api/Comandas.Api/Models/ComandaItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Comandas.Api.Models
+{
+    public class ComandaItem
+    {
+        public int Id { get; set; }
+        public int CardapioItemId { get; set; }
+        public virtual CardapioItem CardapioItem { get; set; }
+        public int ComandaId { get; set; }
+        public virtual Comanda Comanda { get; set; }
+    }
+}
